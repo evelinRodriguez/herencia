ad58314818e304978cfd48be131078fa8da35970
Estudiantes <Estudiantes@Labing70607>
2018-08-29 17:19:58 -0500

    herencia 1.0

---
+++ src/vehiculo/Carro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehiculo;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Carro extends Vehiculoo{
+    void Acelerar(){
+        System.out.println("soy un vehiculo acelerandomm ");
+    }
+   void Ventilar(){
+        System.out.println("soy un vehiculo ventilando ");
+    } 
+}
---
+++ src/vehiculo/Vehiculoo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehiculo;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Vehiculoo {
+    String Marca;
+    double cilindraje;
+    int año;
+    String color;
+    
+    
+    void Acelerar(){
+        System.out.println("soy un vehiculo acelerando ");
+    }
+    void frenar(){
+        System.out.println("soy un vehiculo frenando ");
+    }
+}
